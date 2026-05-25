2846366da40bb0fcb79e583820bbb025862b7cdc
naufalkhoirirobbani-design <naufalkhoirirobbani@gmail.com>
2026-05-25 12:35:10 +0300

    feat: view register and dashboard

--- resources/views/livewire/auth/register.blade.php
+++ resources/views/livewire/auth/register.blade.php
@@ -1,3 +1,60 @@
-<div>
-    {{-- Waste no more time arguing what a good man should be, be one. - Marcus Aurelius --}}
+ <div class="min-h-screen flex items-center justify-center bg-gray-100">
+
+        <form class="bg-white p-8 rounded-2xl shadow-lg w-full max-w-md">
+
+            <h1 class="text-3xl font-bold text-center mb-6">
+                Register
+            </h1>
+
+       
+            <div class="mb-4">
+                <label class="block mb-2 font-medium">
+                    Nama
+                </label>
+
+                <input type="text" placeholder="Masukkan nama"
+                    class="w-full border border-gray-300 rounded-lg p-3 focus:outline-none focus:ring-2 focus:ring-blue-500">
+            </div>
+
+       
+            <div class="mb-4">
+                <label class="block mb-2 font-medium">
+                    Email
+                </label>
+
+                <input type="email" placeholder="Masukkan email"
+                    class="w-full border border-gray-300 rounded-lg p-3 focus:outline-none focus:ring-2 focus:ring-blue-500">
+            </div>
+
+         
+            <div class="mb-6">
+                <label class="block mb-2 font-medium">
+                    Password
+                </label>
+
+                <input type="password" placeholder="Masukkan password"
+                    class="w-full border border-gray-300 rounded-lg p-3 focus:outline-none focus:ring-2 focus:ring-blue-500">
+            </div>
+
+         
+            <button type="submit"
+                class="w-full bg-blue-500 hover:bg-blue-600 text-white font-semibold p-3 rounded-lg transition">
+                Register
+            </button>
+
+            <div class="flex justify-center items-center gap-1 mt-4">
+
+                <p>Sudah punya akun?</p>
+
+                <a href="/login" class="text-blue-500 hover:underline">
+                    Login
+                </a>
+
+                <p>sekarang</p>
+
+            </div>
+
+        </form>
+
+    </div>
 </div>
